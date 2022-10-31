0798b5ec0ab1b6f346bbafe84b55e021353952f3
A-Hesham11 <114056255+A-Hesham11@users.noreply.github.com>
2022-10-31 10:08:51 +0200

    Add files via upload

---
+++ templet.js
@@ -0,0 +1,60 @@
+// The End Of The Year Date To Countdown
+
+let countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
+
+let counter = setInterval(() => {
+
+    let dateNow = new Date().getTime();
+
+    let dateDiff = countDownDate - dateNow;
+
+    let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+    document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+    document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+    document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+    document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+    
+    if (dateDiff < 0) {
+        clearInterval(counter);
+    }
+    }, 1000);
+
+    // Increase Numbers On Scrolling
+
+    
+let progressSpans = document.querySelectorAll(".the-progress span");
+let section = document.querySelector(".our-skills");
+
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false;
+
+window.onscroll = function () {
+
+    if (window.scrollY >= section.offsetTop - 250) {
+        progressSpans.forEach((span) => {
+            span.style.width = span.dataset.width;
+    });
+    }
+
+    if (window.scrollY >= statsSection.offsetTop) {
+        if (!started) {
+            nums.forEach((num) => startCount(num));
+        }
+        started = true;
+        }
+    };
+
+    function startCount(el) {
+        let goal = el.dataset.goal;
+        let count = setInterval(() => {
+            el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(count);
+        }
+        }, 2000 / goal);
+    }
